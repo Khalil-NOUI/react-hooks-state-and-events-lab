64366272c7c120d4a932baaf6b36cccbca7406f8
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-09-07 14:38:41 +0100

    completed assignments

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const [add, setAdd] = useState("Add to Cart");
+
+  function handleAdd() {
+    if (inCart) {
+      setInCart(false);
+      setAdd("Add to Cart");
+    } else {
+      setInCart(true);
+      setAdd("Remove from Cart");
+    }
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAdd}>
+        {add}
+      </button>
     </li>
   );
 }
